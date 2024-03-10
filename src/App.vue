d2a65a0892dcee4ec70a667eb0f5283f3910bce6
AlekseiZam <aleksei.zam2000@yandex.ru>
2024-03-10 13:18:18 +0400

    Complete

--- src/App.vue
+++ src/App.vue
@@ -66,12 +66,87 @@
             <div v-if="err_flag" class="left-line col-7">
               <div class="col-7 cell" v-for="e in code_err" style="cursor: default">{{e.value}}</div>
             </div>
+
             <div class="right-line col-5">
-                <button v-if="!err_flag" class="btn btn-primary mt-1 col-8" style="margin-left: 5px" @click="start2">Продолжить без помех</button>
-                <button v-if="err_flag" class="btn btn-primary mt-1 col-5" style="margin-left: 5px" @click="start2">Внести помеху</button>
+                <button v-if="!err_flag" class="btn btn-primary mt-1 col-8" style="margin-left: 5px" @click="start2(0)">Продолжить без помех</button>
+                <button v-if="err_flag" class="btn btn-primary mt-1 col-5" style="margin-left: 5px" @click="start2(1)">Внести помеху</button>
                 <button v-if="err_flag" class="btn btn-primary mt-1 col-5" style="margin-left: 5px" @click="not_err">Сбросить</button>
             </div>
 
+          </div>
+        </div>
+
+        <div v-if="code_flag" class="col-12 mb-2 mt-4 info">
+          <div class="col-12 cell-line">
+            <div class="title mb-1 p-2">Сообщение на входе приемника</div>
+            <div class="left-line col-7">
+              <div class="col-7 title-cell-info" v-for="k in enter_code">{{k.title}}</div>
+              <br>
+              <div class="col-7 cell" v-for="k in enter_code" v-bind:style="changeColor(k.id), changeColorErr(k.id)" style="cursor: default">{{k.value}}</div>
+              <br>
+              <div class="col-7 title-cell" v-for="k in enter_code">b<sub>{{k.id+1}}</sub></div>
+            </div>
+            <div class="right-line col-5">
+              <div class="text">
+                s1 = b{{ enter_code[0].id + 1 }} ⊕ b{{ enter_code[2].id + 1 }} ⊕
+                b{{ enter_code[4].id + 1 }} ⊕ b{{ enter_code[6].id + 1 }} ⊕  b{{ enter_code[8].id + 1 }} ⊕ b{{ enter_code[10].id + 1 }} =
+                {{ enter_code[0].value }} ⊕ {{ enter_code[2].value }} ⊕ {{ enter_code[4].value }} ⊕ {{ enter_code[6].value }} ⊕ {{ enter_code[8].value }} ⊕
+                {{ enter_code[10].value }} =
+                {{ enter_code[0].value ^ enter_code[2].value ^ enter_code[4].value ^ enter_code[6].value ^ enter_code[8].value ^ enter_code[10].value }}
+                <br>
+                s2 = b{{ enter_code[1].id + 1 }} ⊕ b{{ enter_code[2].id + 1 }} ⊕
+                b{{ enter_code[5].id + 1 }} ⊕ b{{ enter_code[6].id + 1 }} ⊕ b{{ enter_code[9].id + 1 }} ⊕ b{{ enter_code[10].id + 1 }} =
+                {{ enter_code[1].value }} ⊕ {{ enter_code[2].value }} ⊕
+                {{ enter_code[5].value }} ⊕ {{ enter_code[6].value }} ⊕ {{ enter_code[9].value }} ⊕ {{ enter_code[10].value }} =
+                {{ enter_code[1].value ^ enter_code[2].value ^ enter_code[5].value ^ enter_code[6].value ^ enter_code[9].value ^ enter_code[10].value }}
+                <br>
+                s3 = b{{ enter_code[3].id + 1 }} ⊕ b{{ enter_code[4].id + 1 }} ⊕
+                b{{ enter_code[5].id + 1 }} ⊕ b{{ enter_code[6].id + 1 }} ⊕ b{{ enter_code[11].id + 1 }} =
+                {{ enter_code[3].value }} ⊕ {{ enter_code[4].value }} ⊕
+                {{ enter_code[5].value }} ⊕ {{ enter_code[6].value }} ⊕ {{ enter_code[11].value }} =
+                {{ enter_code[3].value ^ enter_code[4].value ^ enter_code[5].value ^ enter_code[6].value ^ enter_code[11].value }}
+                <br>
+                s4 = b{{ enter_code[7].id + 1 }} ⊕ b{{ enter_code[8].id + 1 }} ⊕
+                b{{ enter_code[9].id + 1 }} ⊕ b{{ enter_code[10].id + 1 }} ⊕ b{{ enter_code[11].id + 1 }} =
+                {{ enter_code[7].value }} ⊕ {{ enter_code[8].value }} ⊕
+                {{ enter_code[9].value }} ⊕ {{ enter_code[10].value }} ⊕ {{ enter_code[11].value }} =
+                {{ enter_code[7].value ^ enter_code[8].value ^ enter_code[9].value ^ enter_code[10].value ^ enter_code[11].value }}
+                <br>
+                <div v-if="!no_err_flag">
+                  Ошибка в {{enter_code[0].value ^ enter_code[2].value ^ enter_code[4].value ^ enter_code[6].value ^ enter_code[8].value ^ enter_code[10].value
+                  + (enter_code[1].value ^ enter_code[2].value ^ enter_code[5].value ^ enter_code[6].value ^ enter_code[9].value ^ enter_code[10].value) * 2
+                  + (enter_code[3].value ^ enter_code[4].value ^ enter_code[5].value ^ enter_code[6].value ^ enter_code[11].value) * 4
+                  + (enter_code[7].value ^ enter_code[8].value ^ enter_code[9].value ^ enter_code[10].value ^ enter_code[11].value) * 8}} позициии
+                  <br>
+                  <button v-if="!no_err_flag" class="btn btn-primary mt-1 col-8" style="margin-left: 5px" @click="end">Найти и исправить ошибку</button>
+                </div>
+                 <div v-if="no_err_flag">
+                   Ошибки нет
+                   <br>
+                   Полученное сообщение: <span v-for="a in code">{{a.value}}</span><sub>2</sub>
+                 </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+        <div v-if="end_flag" class="col-12 mb-2 mt-4 info">
+          <div class="col-12 cell-line">
+            <div class="title mb-1 p-2">Исправленное сообщение на входе приемника</div>
+            <div class="left-line col-7">
+              <div class="col-7 title-cell-info" v-for="b in code_hem">{{b.title}}</div>
+              <br>
+              <div class="col-7 cell" v-for="b in code_hem" v-bind:style="changeColor(b.id)" style="cursor: default">{{b.value}}</div>
+              <br>
+              <div class="col-7 title-cell" v-for="b in code_hem">b<sub>{{b.id+1}}</sub></div>
+            </div>
+            <div class="right-line col-5">
+              <div class="text">
+                Полученное сообщение: <span v-for="a in code">{{a.value}}</span><sub>2</sub>
+              </div>
+
+            </div>
           </div>
         </div>
 
@@ -127,6 +202,10 @@ export default {
         {id: 10, value: 0},
         {id: 11, value: 0},
       ],
+      code_flag: false,
+      enter_code: [],
+      no_err_flag: false,
+      end_flag: false
     }
   },
   methods:{
@@ -138,8 +217,16 @@ export default {
         this.code[a.id].value = 0
     },
 
+    not_err(){
+      for(const e in this.code_err)
+        this.code_err[e].value = 0
+      this.err_flag = false
+    },
+
     start()
     {
+      this.code_flag = false
+      this.err_flag = false
       let k = 0
       for (const i in this.code_hem){
         if (((this.code_hem[i].id + 1) & this.code_hem[i].id) != 0)
@@ -166,10 +253,43 @@ export default {
       this.err_flag = true
     },
 
-    not_err(){
-      for(const e in this.code_err)
-        this.code_err[e].value = 0
-      this.err_flag = false
+
+    start2(flag){
+      this.no_err_flag = true
+      this.end_flag = false
+      let enter_code = JSON.parse(JSON.stringify(this.code_hem))
+      if(flag)
+      {
+        for(const k in this.code_err)
+        {
+          if(this.code_err[k].value)
+          {
+            if (enter_code[this.code_err[k].id].value)
+              enter_code[this.code_err[k].id].value = 0
+            else
+              enter_code[this.code_err[k].id].value = 1
+          }
+        }
+        this.no_err_flag = false
+      }
+      this.enter_code = enter_code
+      this.code_flag = true
+    },
+
+    changeColorErr(i)
+    {
+      if (i == ((this.enter_code[0].value ^ this.enter_code[2].value ^ this.enter_code[4].value ^ this.enter_code[6].value ^ this.enter_code[8].value ^ this.enter_code[10].value
+          + (this.enter_code[1].value ^ this.enter_code[2].value ^ this.enter_code[5].value ^ this.enter_code[6].value ^ this.enter_code[9].value ^ this.enter_code[10].value) * 2
+          + (this.enter_code[3].value ^ this.enter_code[4].value ^ this.enter_code[5].value ^ this.enter_code[6].value ^ this.enter_code[11].value) * 4
+          + (this.enter_code[7].value ^ this.enter_code[8].value ^ this.enter_code[9].value ^ this.enter_code[10].value ^ this.enter_code[11].value) * 8) - 1))
+        return "background: red"
+    },
+
+    end()
+    {
+      this.code_flag = false
+      this.end_flag = true
+
     }
 
   }
